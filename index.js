834687012f0618d47c11d1621f147c9a57b31996
Rishabh7778 <saty665566@gmai.com>
2025-01-03 13:06:47 +0530

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import express from "express";
-import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from 'cors';
-import Booking from '../backend/database/db.js';
+import db from '../backend/database/db.js';
 
 
 dotenv.config();
@@ -15,12 +14,12 @@ app.use(cors());
 app.post("/api/bookings", async (req, res) => {
     const { date, time, guests, name, contact } = req.body;
   
-    const existingBooking = await Booking.findOne({ date, time });
+    const existingBooking = await db.findOne({ date, time });
     if (existingBooking) {
       return res.status(400).json({ message: "Time slot already booked." });
     }  
     try {
-      const booking = new Booking({ date, time, guests, name, contact });
+      const booking = new db({ date, time, guests, name, contact });
       await booking.save();
       res.status(201).json({ message: "Booking created successfully!", booking });
     } catch (err) {
@@ -30,7 +29,7 @@ app.post("/api/bookings", async (req, res) => {
 
   app.get("/api/bookings", async (req, res) => {
     try {
-      const bookings = await Booking.find();
+      const bookings = await db.find();
       res.status(200).json(bookings);
     } catch (err) {
       res.status(500).json({ message: "Error fetching bookings.", error: err });
@@ -40,7 +39,7 @@ app.post("/api/bookings", async (req, res) => {
   app.delete("/api/bookings/:id", async (req, res) => {
     const { id } = req.params;
     try {
-      await Booking.findByIdAndDelete(id);
+      await db.findByIdAndDelete(id);
       res.status(200).json({ message: "Booking deleted successfully!" });
     } catch (err) {
       res.status(500).json({ message: "Error deleting booking.", error: err });
