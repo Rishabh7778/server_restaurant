294ec8aec8c8be242930b5fe9429c0e20b0f52b5
Rishabh7778 <saty665566@gmai.com>
2025-01-03 17:05:28 +0530

    db fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from 'cors';
-import db from "./database/db";
+import db from "./database/db.js";
 
 
 dotenv.config();
